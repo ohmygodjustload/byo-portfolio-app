6d68e2cf3643197b981a66e7b498321bee8aba8e
Andrew Peirce <drew101peirce@gmail.com>
2026-04-08 18:02:44 -0500

    shit

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL("https://cdn.sanity.io/**")],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        pathname: "/images/**",
+      },
+    ],
   },
 };
 
